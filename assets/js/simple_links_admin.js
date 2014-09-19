e60d7afcd4b6342dfd08a5947d26018f97b33532
lipemat <mat@matlipe.com>
2014-09-19 12:50:11 -0500

    clean up js format

--- assets/js/simple_links_admin.js
+++ assets/js/simple_links_admin.js
@@ -8,33 +8,33 @@
 
 var isSimpleLinks = false;
 
-jQuery( function ($) {
+jQuery( function( $ ){
 
 	//A boolean to make sure we are on the right page
-	if( $('input[type="hidden"][name="post_type"]').val() == 'simple_link' ){
+	if( $( 'input[type="hidden"][name="post_type"]' ).val() == 'simple_link' ){
 		isSimpleLinks = true;
 	} else {
-	    if( $('input[type="hidden"][name="taxonomy"]').val() == 'simple_link_category' ){
-	        isSimpleLinks = true;
-	    }
+		if( $( 'input[type="hidden"][name="taxonomy"]' ).val() == 'simple_link_category' ){
+			isSimpleLinks = true;
+		}
 	}
 
 	//easter egg
-	$('.simple-links-title').change(function(){
-		if( $(this).val() == "Simple Links" ){
+	$( '.simple-links-title' ).change( function(){
+		if( $( this ).val() == "Simple Links" ){
 			for( var i = 0; i < 10; i++ ){
-				$(this).css({'box-shadow': '0px 0px 10px '+i+'px yellow'});
+				$( this ).css( {'box-shadow' : '0px 0px 10px ' + i + 'px yellow'} );
 			}
-			$(this).after( '<h2><center>HALLELUJAH!!</center></h2>' );
+			$( this ).after( '<h2><center>HALLELUJAH!!</center></h2>' );
 		}
-	});
+	} );
 
 
-    SLcatPage.init(isSimpleLinks);
+	SLcatPage.init( isSimpleLinks );
 	SLsortPage.init();
 	SLsettings.init();
 
-});
+} );
 
 var $s = jQuery.noConflict();
 
@@ -44,12 +44,14 @@ var $s = jQuery.noConflict();
  * @since 9/24/12
  */
 var SLcatPage = {
-   init: function( good ){
-       if( !good ) return
-       //Remove the slug section for no conflicts later
-      $s('#tag-slug, label[for="tag-slug"], #addtag div p').remove();
-      $s('#edittag input[id="slug"], label[for="slug"], #edittag p.description').remove();
-  }
+	init : function( good ){
+		if( !good ){
+			return
+		}
+		//Remove the slug section for no conflicts later
+		$s( '#tag-slug, label[for="tag-slug"], #addtag div p' ).remove();
+		$s( '#edittag input[id="slug"], label[for="slug"], #edittag p.description' ).remove();
+	}
 
 };
 
@@ -59,17 +61,14 @@ var SLcatPage = {
  * @since 8/17/12
  */
 var SLsettings = {
-		init: function(){
-			//Add another Row for the additional fields
-			$s('#simple-link-additional').click( function(){
-				$s('#link-additional-placeholder').after( $s('#link-extra-field').html() );
-				return false;
-			});
-		}
+	init : function(){
+		//Add another Row for the additional fields
+		$s( '#simple-link-additional' ).click( function(){
+			$s( '#link-additional-placeholder' ).after( $s( '#link-extra-field' ).html() );
+			return false;
+		} );
+	}
 };
-
-
-
 
 
 /**
@@ -77,64 +76,68 @@ var SLsettings = {
  * @since 1.23.14
  */
 var SLsortPage = {
-		init : function(){
-		    if( $s( '.draggable-children' ).length < 1 ) return;
+	init : function(){
+		if( $s( '.draggable-children' ).length < 1 ){
+			return;
+		}
 
-			  //Settup the Draggable list
-		    $s( '.draggable-children' ).sortable( {
-		  	  							placeholder: 'sortable-placeholder menu-item-depth-1',
-		  	  							stop: function(){ SLsortPage.sort( $s(this).attr('id') ); }
-		    });
-
-
-		    //the filter by Categories
-		    $s('#SL-sort-cat').change( function(){
-		    	SLsortPage.catFilter( $s(this).val() );
-		    });
-		},
-
-		/**
-		 * Runs the ajax with the new link order
-		 * @param string linkID the id of the sortable list
-		 * @since 8/15/12
-		 */
-		sort :function( linkID ){
-	    	//Get the new sort order
-	    	var data = $s( 'ul#'+linkID ).sortable("serialize");
-
-	    	$s.post(SLajaxURL.sortURL, data, function(respon){});
-	    },
+		//Settup the Draggable list
+		$s( '.draggable-children' ).sortable( {
+			placeholder : 'sortable-placeholder menu-item-depth-1', stop : function(){
+				SLsortPage.sort( $s( this ).attr( 'id' ) );
+			}
+		} );
 
 
-	    /**
-	     * Hide all items on the list that are not in the selected category
-	     * @param string slug the categories slug
-	     * @since 8/15/12
-	     */
-	    catFilter : function(slug){
+		//the filter by Categories
+		$s( '#SL-sort-cat' ).change( function(){
+			SLsortPage.catFilter( $s( this ).val() );
+		} );
+	},
 
-	    	//To Reset the category sort
-	    	if( slug == 'Xall-catsX' ){
-	    		$s('#SL-drag-ordering li').show('slow');
-	    		$s('#SL-drag-ordering li').each( function(){
-		    		cleanID = $s(this).attr('id').replace(/x/g,'');
-		    		$s(this).attr({'id': cleanID });
-		    	});
+	/**
+	 * Runs the ajax with the new link order
+	 * @param string linkID the id of the sortable list
+	 * @since 8/15/12
+	 */
+	sort : function( linkID ){
+		//Get the new sort order
+		var data = $s( 'ul#' + linkID ).sortable( "serialize" );
 
-	    		return;
-	    	}
+		$s.post( SLajaxURL.sortURL, data, function( respon ){
+		} );
+	},
 
-	    	//Show and fix the id of all list items in this category by slug
-	    	$s('#SL-drag-ordering li.'+slug).show('slow');
-	    	$s('#SL-drag-ordering li.'+slug).each( function(){
-	    		cleanID = $s(this).attr('id').replace(/x/g,'');
-	    		$s(this).attr({'id': cleanID });
-	    	});
 
-	    	//Hide and break the id of all the list items not in this category
-	    	$s('#SL-drag-ordering li').not('.'+slug).hide('slow');
-	    	$s('#SL-drag-ordering li').not('.'+slug).each( function(){
-	    		$s(this).attr({'id': 'x'+$s(this).attr('id')});
-	    	});
-	    }
+	/**
+	 * Hide all items on the list that are not in the selected category
+	 * @param string slug the categories slug
+	 * @since 8/15/12
+	 */
+	catFilter : function( slug ){
+
+		//To Reset the category sort
+		if( slug == 'Xall-catsX' ){
+			$s( '#SL-drag-ordering li' ).show( 'slow' );
+			$s( '#SL-drag-ordering li' ).each( function(){
+				cleanID = $s( this ).attr( 'id' ).replace( /x/g, '' );
+				$s( this ).attr( {'id' : cleanID} );
+			} );
+
+			return;
+		}
+
+		//Show and fix the id of all list items in this category by slug
+		$s( '#SL-drag-ordering li.' + slug ).show( 'slow' );
+		$s( '#SL-drag-ordering li.' + slug ).each( function(){
+			cleanID = $s( this ).attr( 'id' ).replace( /x/g, '' );
+			$s( this ).attr( {'id' : cleanID} );
+		} );
+
+		//Hide and break the id of all the list items not in this category
+		$s( '#SL-drag-ordering li' ).not( '.' + slug ).hide( 'slow' );
+		$s( '#SL-drag-ordering li' ).not( '.' + slug ).each( function(){
+			$s( this ).attr( {'id' : 'x' + $s( this ).attr( 'id' )} );
+		} );
+	}
 };
--- assets/js/simple_links_settings.js
+++ assets/js/simple_links_settings.js
@@ -6,20 +6,20 @@
  *
  */
 
-jQuery(function($) {
+jQuery( function( $ ){
 	// close postboxes that should be closed
-	$('.if-js-closed').removeClass('if-js-closed').addClass('closed');
+	$( '.if-js-closed' ).removeClass( 'if-js-closed' ).addClass( 'closed' );
 
 	// postboxes setup  These need to be set the handlers (screens) of the post boxes
-	postboxes.add_postbox_toggles('sl-settings-boxes');
-	
-	$('.link_delete_additional').click( function(){
-		$(this).parent().remove();
-	});
+	postboxes.add_postbox_toggles( 'sl-settings-boxes' );
+
+	$( '.link_delete_additional' ).click( function(){
+		$( this ).parent().remove();
+	} );
 
 	SLsettingsAjax.init();
 	SLsettingsQtips.init();
-});
+} );
 
 var $s = jQuery.noConflict();
 
@@ -30,12 +30,12 @@ var $s = jQuery.noConflict();
  *
  */
 var SLsettingsAjax = {
-	init : function() {
+	init : function(){
 		//the import links ajax
-		$s('#sl-import-links').click(function() {
+		$s( '#sl-import-links' ).click( function(){
 			SLsettingsAjax.importLinks();
 			return false;
-		});
+		} );
 
 	},
 
@@ -43,14 +43,14 @@ var SLsettingsAjax = {
 	 * Make the request to import the links
 	 *
 	 */
-	importLinks : function() {
+	importLinks : function(){
 
 		var data = '';
-		$s('#sl-import-loading').show();
-		$s.post(SLajaxURL.importLinksURL, data, function(respon) {
-			$s('#sl-import-loading').hide();
-			$s('#import-links-success').slideDown('slow');
-		});
+		$s( '#sl-import-loading' ).show();
+		$s.post( SLajaxURL.importLinksURL, data, function( respon ){
+			$s( '#sl-import-loading' ).hide();
+			$s( '#import-links-success' ).slideDown( 'slow' );
+		} );
 
 	}
 };
@@ -64,59 +64,43 @@ var SLsettingsAjax = {
  */
 var SLsettingsQtips = {
 
-	init : function() {
+	init : function(){
 
 		//The Hide Ordering Option
-		$s('#SL-hide-ordering').qtip({
-			'content' : 'This will prevent editors from using the drag and drop ordering.',
-			style : {
+		$s( '#SL-hide-ordering' ).qtip( {
+			'content' : 'This will prevent editors from using the drag and drop ordering.', style : {
 				border : {
-					width : 1,
-					radius : 8,
-					color : '#21759B'
-				},
-				tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
+					width : 1, radius : 8, color : '#21759B'
+				}, tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
 			}
-		});
+		} );
 
 		//The show setting option
-		$s('#SL-show-settings').qtip({
-			'content' : 'This will allow editors access to this Settings Page.',
-			style : {
+		$s( '#SL-show-settings' ).qtip( {
+			'content' : 'This will allow editors access to this Settings Page.', style : {
 				border : {
-					width : 1,
-					radius : 8,
-					color : '#21759B'
-				},
-				tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
+					width : 1, radius : 8, color : '#21759B'
+				}, tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
 			}
-		});
+		} );
 
 		//The remove links option
-		$s('#SL-remove-links').qtip({
-			'content' : 'This will remove all traces of the Wordpres built in links',
-			style : {
+		$s( '#SL-remove-links' ).qtip( {
+			'content' : 'This will remove all traces of the Wordpres built in links', style : {
 				border : {
-					width : 1,
-					radius : 8,
-					color : '#21759B'
-				},
-				tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
+					width : 1, radius : 8, color : '#21759B'
+				}, tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
 			}
-		});
+		} );
 
 		//The import links
-		$s('#SL-import-links').qtip({
-			content : 'This will import all existing Wordpress Links into the Simple Links',
-			style : {
+		$s( '#SL-import-links' ).qtip( {
+			content : 'This will import all existing Wordpress Links into the Simple Links', style : {
 				border : {
-					width : 1,
-					radius : 8,
-					color : '#21759B'
-				},
-				tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
+					width : 1, radius : 8, color : '#21759B'
+				}, tip : 'topLeft' // Notice the corner value is identical to the previously mentioned positioning corners
 			}
-		});
+		} );
 	}
 };
 

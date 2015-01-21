78058a8b564981761fd9a134d5c7ffb679929b22
lipemat <mat@matlipe.com>
2015-01-21 10:41:30 -0500

    create new template tags for accessing the simple_links and simple links admin classes Convert some uses over to the new functions

--- template-tags.php
+++ template-tags.php
@@ -8,6 +8,38 @@
  */
 
 /**
+ * simple_links_admin
+ *
+ *
+ * @return \simple_links_admin
+ */
+function simple_links_admin(){
+	global $simple_links_admin_func;
+	if( empty( $simple_links_admin_func ) ){
+		$simple_links_admin_func = new simple_links_admin();
+	}
+	return $simple_links_admin_func;
+}
+
+/**
+ * simple_links
+ *
+ *
+ * @return \simple_links
+ */
+function simple_links(){
+	global $simple_links;
+	if( empty( $simple_links ) ){
+		return $simple_links = new simple_links();
+	}
+
+	return $simple_links;
+}
+
+
+
+
+/**
  * Simple Links Questions
  *
  * Creates a question mark icon of the tooltips
@@ -17,5 +49,4 @@
  */
 function simple_links_questions( $id ){
 	printf( ' <img src="%squestion.png" id="%s">', SIMPLE_LINKS_IMG_DIR, $id );
-
 }

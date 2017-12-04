7988254e54eb5b92a640588546fd24a4724183b4
lipemat <mat@matlipe.com>
2017-12-04 10:49:01 -0500

    update unit tests for new simple-links-pro

--- dev/bootstrap.php
+++ dev/bootstrap.php
@@ -1,15 +1,11 @@
 <?php
 
 $GLOBALS['wp_tests_options']['active_plugins'][] = 'simple-links/simple-links.php';
-$GLOBALS['wp_tests_options']['active_plugins'][] = 'simple-links-search/simple-links-search.php';
-$GLOBALS['wp_tests_options']['active_plugins'][] = 'simple-links-display-by-category/simple-links-display-by-category.php';
-$GLOBALS['wp_tests_options']['active_plugins'][] = 'simple-links-csv-import/simple-links-csv-import.php
-';
-
+$GLOBALS['wp_tests_options']['active_plugins'][] = 'simple-links-pro/simple-links-pro.php';
 $GLOBALS['wp_tests_options']['permalink_structure'] = '%postname%/';
 
-require( 'wp-tests-config.php' );
+require dirname( __FILE__ ) . '/wp-tests-config.php';
 
-global $wp_version; // wp's test suite doesn't globalize this, but we depend on it for loading core
+global $wp_version;
 
 require 'E:/SVN/wordpress-tests/includes/bootstrap-no-install.php';
